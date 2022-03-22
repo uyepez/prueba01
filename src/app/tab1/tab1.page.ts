4685bb827b83e84e93f45545f388978b71fc5dc2
uyepez <uyepez@tvp.mx>
2022-03-22 11:45:47 -0600

    camara configurada

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { Camera, CameraResultType } from '@capacitor/camera';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,9 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+
+  capturedImage = null;
 
   constructor( private alertController: AlertController) {}
 
@@ -24,6 +29,22 @@ export class Tab1Page {
 
     const { role } = await alert.onDidDismiss();
     console.log('onDidDismiss resolved with role', role);
+  }
+
+  //acceso a la camara
+  async addImage(){
+    const image = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: false,
+    resultType: CameraResultType.Base64,
+    promptLabelCancel: "Cancelar",
+    promptLabelPicture:"Camara",
+    promptLabelPhoto: "Galeria"
+  });
+
+  console.log('resultado de la foto: ', image);
+  this.capturedImage = `data:image/jpge;base64,${image.base64String}`;
+
   }
 
 }
